11aeebcfafa83fdec56e8a4379698ebd93c96090
Ryan Silvera <ryan.silvera@icloud.com>
2022-09-19 17:57:35 -0400

    Add most of my necessary functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+// using constant type for the choices variable, have the required strings in it
+const choices =['rock', 'paper', 'scissors']
+
+// function for the computer to randomly return either Rock, Paper, or Scissors
+function getComputerChoice(choices) {
+    return choices [Math.floor(Math.random()*choices.length)]
+}
+console.log(getComputerChoice(choices))
+
+// function for playing a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection){
+    
+}
+
+const playerSelection = prompt("Whats your move?")
+const computerSelection = getComputerChoice(choices);
