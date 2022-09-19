8fc94f97b8c63c14af556b1f5b187778c7b8394d
Ryan Silvera <ryan.silvera@icloud.com>
2022-09-19 19:08:37 -0400

    Most of the functions working
    Need to figure out why my playRound function isnt working as
    intended

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 // using constant type for the choices variable, have the required strings in it
-const choices =['rock', 'paper', 'scissors']
+const choices =['rock', 'paper', 'scissors'];
 
 // function for the computer to randomly return either Rock, Paper, or Scissors
-function getComputerChoice(choices) {
-    return choices [Math.floor(Math.random()*choices.length)]
+function getComputerChoice() {
+    const decision = choices[Math.floor(Math.random()*choices.length)];
+    return decision
 }
-console.log(getComputerChoice(choices))
 
+// function for checking the winner
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        return "Tie"
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
 // function for playing a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection){
-
-}
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie") {
+        return "Its a tie!"
+    }
+    else if (result  == "Player") {
+        return  'Goodjob! ${playerSelection} beats ${computerSelection}. You won'
+    }
+    else {
+        return 'Oh no! ${computerSelection} beats ${playerSelection}. You lost'
+    }
+}  
 
 // the parameters for my playRound function
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice(choices);
-
-function getPlayerChoice() {
-    const input = prompt("Pick between Rock, Paper or Scissor");
-    input = input.toLowerCase();
-}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
